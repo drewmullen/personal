f7d97ff268dea974f49461fcbd98129ec6b31120
Mullen, Richard (Drew) <drew.mullen@ge.com>
2017-09-07 09:11:11 -0400

    added more functions for modularity

--- python-training/hangman.py
+++ python-training/hangman.py
@@ -1,29 +1,44 @@
 import random
-#pick a random word from a list
-#user guesses letters in the word
-word_list = ['one','two','three']
 
-def main():
+
+def setup():
+    word_list = ['one','two','three']
     #select word from word_list, set to lowercase, and add it as a list
     secret_word = []
-    secret_word = list(word_list[random.randint(1,2)].lower())
+    secret_word = list(word_list[random.randint(0,2)].lower())
 
     #build list with _s
     correct_guesses = []
     correct_guesses.extend('_' * len(secret_word))
+    game(secret_word, correct_guesses)
 
-#accept input
+#prompt and accept input
 def prompt():
     guess = input("Enter a guess: ").lower()
+    if len(guess) != 1:
+        print("One character only, please!")
+        prompt()
     return guess
 
 #compare input to secret word
-def compare_prompt(secret_word, guess):
+def compare_prompt(secret_word, guess, correct_guesses):
     for letter in secret_word:
         if guess == letter:
             #add letter to correct spot in status list by finding the index location in secret word
             correct_guesses[int(secret_word.index(letter))] = guess
-            #find index of letter in secret_word and make correct_guesses correlating index the guess
+            print("You got one!")
 
 #output status
-    #print(*correct_guesses)
+def output_status(correct_guesses):
+    print(*correct_guesses)
+
+def game(secret_word, correct_guesses):
+    counter=0
+    while correct_guesses!= secret_word:
+        output_status(correct_guesses)
+        guess = prompt()
+        compare_prompt(secret_word,guess, correct_guesses)
+        counter+=1
+    print("You won! It took you {} guesses.".format(counter))
+
+setup()
